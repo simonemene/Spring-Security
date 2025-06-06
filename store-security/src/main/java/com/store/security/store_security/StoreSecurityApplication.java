24133b630c5a854501b7af36cc1b52cdb37e2650
Simone <persolenom@gmail.com>
2025-06-06 18:58:08 +0200

    INIT COMMIT

---
+++ store-security/src/main/java/com/store/security/store_security/StoreSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.store.security.store_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreSecurityApplication.class, args);
+	}
+
+}
---
+++ store-security/src/test/java/com/store/security/store_security/StoreSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.security.store_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
