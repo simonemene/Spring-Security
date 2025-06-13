8f5802524533ffbc1ee482fa64e9bdeb595b3d03
simone <s.meneghetti7@gmail.com>
2025-06-13 15:34:57 +0200

    sistemazione test

--- store-security/backend/src/test/java/com/store/security/store_security/StoreSecurityApplicationTests.java
+++ store-security/backend/src/test/java/com/store/security/store_security/StoreSecurityApplicationTests.java
@@ -4,10 +4,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties =
-		{
-				"spring.jpa.hibernate.ddl-auto=create-drop"
-		})
 public class StoreSecurityApplicationTests {
 
 
