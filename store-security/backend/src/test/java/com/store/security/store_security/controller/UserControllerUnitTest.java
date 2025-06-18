4e54e26a112aae8b5aba2b10d3b3dd05428fd104
simone <s.meneghetti7@gmail.com>
2025-06-18 16:37:12 +0200

    test user controller

--- store-security/backend/src/test/java/com/store/security/store_security/controller/UserControllerUnitTest.java
+++ store-security/backend/src/test/java/com/store/security/store_security/controller/UserControllerUnitTest.java
@@ -1,14 +1,20 @@
 package com.store.security.store_security.controller;
 
 import com.store.security.store_security.controladvice.GenericExceptionHandler;
-import com.store.security.store_security.service.IUserService;
+import com.store.security.store_security.exceptions.UserException;
+import com.store.security.store_security.service.UserService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+//TODO:not real UNIT TEST
 
 @WebMvcTest(UserController.class)
 @Import(GenericExceptionHandler.class)
@@ -17,17 +23,20 @@ public class UserControllerUnitTest {
 	@Autowired
 	private MockMvc mockMvc;
 
-	@Mock
-	private IUserService userService;
+	@MockitoBean
+	private UserService userService;
 
 
 	@Test
-	@WithMockUser(username = "admin@gmail.com", roles = "ADMIN")
-	public void userNotFound()
-	{
+	@WithMockUser(username = "admin@gmail.com", roles = "USER")
+	public void userNotFound() throws Exception {
 		//given
 		String username = "admin@gmail.com";
+		Mockito.when(userService.findUser(username)).thenThrow(new UserException("User admin not found"));
 		//when
 		//then
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/user/getUserDetails/{username}",username))
+				.andExpect(MockMvcResultMatchers.status().isBadRequest())
+				.andExpect(MockMvcResultMatchers.content().string("User admin not found"));
 	}
 }
