ed51f749aba7126a00655d1d315a4c1c6cd90054
simone <s.meneghetti7@gmail.com>
2025-06-18 16:29:57 +0200

    test user controller

--- store-security/backend/src/test/java/com/store/security/store_security/controller/UserControllerUnitTest.java
+++ store-security/backend/src/test/java/com/store/security/store_security/controller/UserControllerUnitTest.java
@@ -1,4 +1,33 @@
 package com.store.security.store_security.controller;
 
+import com.store.security.store_security.controladvice.GenericExceptionHandler;
+import com.store.security.store_security.service.IUserService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(UserController.class)
+@Import(GenericExceptionHandler.class)
 public class UserControllerUnitTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Mock
+	private IUserService userService;
+
+
+	@Test
+	@WithMockUser(username = "admin@gmail.com", roles = "ADMIN")
+	public void userNotFound()
+	{
+		//given
+		String username = "admin@gmail.com";
+		//when
+		//then
+	}
 }
