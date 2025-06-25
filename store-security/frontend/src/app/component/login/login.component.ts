c1acdceea9ec1205060ea77d34479b2f0fe5547e
simone <s.meneghetti7@gmail.com>
2025-06-25 12:30:43 +0200

    login

---
+++ store-security/frontend/src/app/component/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { AuthenticationService } from '../../service/authentication.service';
+import { UserDto } from '../../model/UserDto';
+import { FormControl, FormGroup, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+  user!:UserDto;
+  storeForm!:FormGroup;
+
+  constructor(private auth:AuthenticationService)
+  {
+    this.user = new UserDto();
+    this.storeForm = new FormGroup(
+      {
+        email: new FormControl('',[Validators.required,Validators.email]),
+        password : new FormControl('',[Validators.required])
+      }
+    )
+  }
+
+  onSubmit()
+  {}
+
+
+}
