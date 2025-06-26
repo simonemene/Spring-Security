6fcf3973d039a58d5b190148f197831dc880c040
simone <s.meneghetti7@gmail.com>
2025-06-26 16:48:02 +0200

    admin manage

--- store-security/frontend/src/app/component/manage-article/manage-article.component.ts
+++ store-security/frontend/src/app/component/manage-article/manage-article.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { StockService } from '../../service/stock.service';
+import { ArticleService } from '../../service/article.service';
+import { StockDto } from '../../model/StockDto';
 
 @Component({
   selector: 'app-manage-article',
@@ -7,6 +10,23 @@ import { Component } from '@angular/core';
   templateUrl: './manage-article.component.html',
   styleUrl: './manage-article.component.scss'
 })
-export class ManageArticleComponent {
+export class ManageArticleComponent implements OnInit{
+
+  stockService = inject(StockService);
+  articleService = inject(ArticleService);
+
+  allStock:StockDto[] = [];
+
+  constructor()
+  {}
+  
+
+  ngOnInit(): void {
+    
+  }
+
+
+
+  
 
 }
