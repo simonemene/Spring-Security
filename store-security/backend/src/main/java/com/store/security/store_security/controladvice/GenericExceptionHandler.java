894b5a0db6414c3a5c83d3cc47ee8b33d4a59b04
simone <s.meneghetti7@gmail.com>
2025-07-03 16:37:09 +0200

    valid

--- store-security/backend/src/main/java/com/store/security/store_security/controladvice/GenericExceptionHandler.java
+++ store-security/backend/src/main/java/com/store/security/store_security/controladvice/GenericExceptionHandler.java
@@ -9,11 +9,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.lang.Nullable;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GenericExceptionHandler extends ResponseEntityExceptionHandler {
@@ -55,7 +61,15 @@ public class GenericExceptionHandler extends ResponseEntityExceptionHandler {
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(
 			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
-		return this.handleExceptionInternal(ex, (Object)null, headers, status, request);
+		Map<String,String> errors = new HashMap<>();
+		List<ObjectError> error = ex.getBindingResult().getAllErrors();
+		for(ObjectError objectError : error)
+		{
+			String field = ((FieldError) error) .getField();
+			String message = objectError.getDefaultMessage();
+			errors.put(field,message);
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
 	}
 
 
