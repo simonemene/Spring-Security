ad11017c0cb400243695c2acfe5cab3c9f3fe792
simone <s.meneghetti7@gmail.com>
2025-07-03 16:37:41 +0200

    valid

--- store-security/backend/src/main/java/com/store/security/store_security/dto/StockDto.java
+++ store-security/backend/src/main/java/com/store/security/store_security/dto/StockDto.java
@@ -1,7 +1,5 @@
 package com.store.security.store_security.dto;
 
-import com.store.security.store_security.entity.ArticleEntity;
-import com.store.security.store_security.entity.StockArticleEntity;
 import lombok.Builder;
 import lombok.Data;
 
