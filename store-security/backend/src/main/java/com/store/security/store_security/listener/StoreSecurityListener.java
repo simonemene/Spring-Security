b26f76a913adb7388b7e2ff84acb8871a593860e
simone <s.meneghetti7@gmail.com>
2025-07-11 16:13:27 +0200

    logout handler

--- store-security/backend/src/main/java/com/store/security/store_security/listener/StoreSecurityListener.java
+++ store-security/backend/src/main/java/com/store/security/store_security/listener/StoreSecurityListener.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.authentication.event.LogoutSuccessEvent;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -20,7 +21,13 @@ public class StoreSecurityListener {
     @EventListener
     public void onAuthenticationFailureEvent(AbstractAuthenticationFailureEvent failure)
     {
-        log.info("Authentication failure");
+        log.info("Authentication failure for: {}",failure.getAuthentication().getName());
+
     }
 
+    @EventListener
+    public void onLogoutSuccessEvent(LogoutSuccessEvent logoutSuccessEvent)
+    {
+        log.info("Logout success for: {}",logoutSuccessEvent.getAuthentication().getName());
+    }
 }
